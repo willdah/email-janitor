c7aae2ad96e79d590bd9e8a0f3a53510c29e5e17
willdah <willdahern@gmail.com>
2026-01-23 21:10:26 -0500

    feat: update root_agent model and integrate get_unread_emails function in agent.py; add gmail_client.py for Gmail functionality

--- email_janitor/agent.py
+++ email_janitor/agent.py
@@ -1,8 +1,12 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools import FunctionTool
+from .gmail_client import get_unread_emails
+
 
 root_agent = Agent(
-    model="<FILL_IN_MODEL>",
+    model="gemini-2.5-flash",
     name="root_agent",
     description="A helpful assistant for user questions.",
     instruction="Answer user questions to the best of your knowledge",
+    tools=[FunctionTool(get_unread_emails)],
 )
