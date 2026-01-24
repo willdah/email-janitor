859a439396fa986791cc3b238bae7c6afdaf0a8d
billybuckle <willdahern@gmail.com>
2026-01-23 21:03:05 -0500

    feat: integrate simplegmail for Gmail client functionality and update dependencies in pyproject.toml

---
+++ email_janitor/tools/gmail_client.py
@@ -0,0 +1,26 @@
+from simplegmail import Gmail
+from simplegmail.message import Message
+
+
+gmail: Gmail | None = None
+
+def get_gmail_client():
+  """
+  Returns a Gmail client instance. If the client is not initialized, it will be initialized using the client_secret.json file.
+  """
+  global gmail
+  if gmail is None:
+    gmail = Gmail()
+  return gmail
+
+
+def get_unread_emails() -> list[Message]:
+  """
+  Uses the Gmail client to get a list of unread emails.
+  """
+  gmail: Gmail = get_gmail_client()
+  unread_emails: list[Message] = gmail.get_unread_messages()
+  return unread_emails
+
+
+print(get_unread_emails())
