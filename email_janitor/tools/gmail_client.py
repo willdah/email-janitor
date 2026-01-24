fd2769f6c4ed5d26fb55b62361532d06ca406c7a
billybuckle <willdahern@gmail.com>
2026-01-23 21:03:21 -0500

    style: format code in agent.py and gmail_client.py for consistency

--- email_janitor/tools/gmail_client.py
+++ email_janitor/tools/gmail_client.py
@@ -4,23 +4,24 @@ from simplegmail.message import Message
 
 gmail: Gmail | None = None
 
+
 def get_gmail_client():
-  """
-  Returns a Gmail client instance. If the client is not initialized, it will be initialized using the client_secret.json file.
-  """
-  global gmail
-  if gmail is None:
-    gmail = Gmail()
-  return gmail
+    """
+    Returns a Gmail client instance. If the client is not initialized, it will be initialized using the client_secret.json file.
+    """
+    global gmail
+    if gmail is None:
+        gmail = Gmail()
+    return gmail
 
 
 def get_unread_emails() -> list[Message]:
-  """
-  Uses the Gmail client to get a list of unread emails.
-  """
-  gmail: Gmail = get_gmail_client()
-  unread_emails: list[Message] = gmail.get_unread_messages()
-  return unread_emails
+    """
+    Uses the Gmail client to get a list of unread emails.
+    """
+    gmail: Gmail = get_gmail_client()
+    unread_emails: list[Message] = gmail.get_unread_messages()
+    return unread_emails
 
 
 print(get_unread_emails())
